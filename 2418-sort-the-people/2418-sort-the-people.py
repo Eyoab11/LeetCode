984749000bac965f15d8a8217754c4a7fd328ac9
Eyoab11 <134016127+Eyoab11@users.noreply.github.com>
2024-03-27 18:45:32 +0300

    Time: 289 ms (16.29%), Space: 17.1 MB (77.30%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,5 +1,11 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        nums = list(range(len(heights)))
-        nums.sort(key=lambda i: -heights[i])
-        return [names[i] for i in nums]
+        n = len(names)
+        for i in range(n - 1):
+            last = i
+            for j in range(i + 1, n):
+                if heights[j] > heights[last]:
+                    last = j
+            names[i], names[last] = names[last], names[i]
+            heights[i], heights[last] = heights[last], heights[i]
+        return names
