50c4c6bf1970013e8d1f18c6d2633c03ec6fd3ad
Eyoab11 <134016127+Eyoab11@users.noreply.github.com>
2024-05-14 02:59:20 +0300

    Time: 467 ms (5.03%), Space: 28.2 MB (97.02%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,8 +4,6 @@ class Solution:
         nums.sort()
         res = []
         for i in range(len(nums)-1):
-            if nums[i] < nums[i + 1]:
-                pass
-            else:
+            if nums[i] == nums[i + 1]:
                 count = 1 
         return count == 1
